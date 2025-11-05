3fd5e0193f482328c1022991bcf933b3400e4e1c
199Benji <benjaminmiracle332@gmail.com>
2025-11-05 08:09:05 +0100

    owners commit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import MainLayout from './layout/MainLayout'
+import { BrowserRouter } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+        <MainLayout />
+    </BrowserRouter>
     </>
   )
 }
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import AppRoutes from '../AppRoutes';
+
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <>
+      <Navbar />
+      <AppRoutes />
+      <Footer />
+    </>
   )
 }
 
