471f8eb75bf919ee5471f4b166f85904babce1d1
Veekruz <akpane923@gmail.com>
2025-11-07 16:45:07 +0100

    Esther is the shortest person i know

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Information from '../components/Information'
+import Items from '../components/Items'
+import Request from '../components/Request'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Information />
+      <Items />
+      <Request />
+    </>
   )
 }
 
